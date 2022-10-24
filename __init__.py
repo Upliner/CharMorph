10026b56ab61db7abd7451eecf94b0ae5fe8bb5b
Michael Vigovsky <upliner@gmail.com>
2022-10-25 01:41:43 +0300

    Bump version

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ logger = logging.getLogger(__name__)
 bl_info = {
     "name": "CharMorph",
     "author": "Michael Vigovsky",
-    "version": (0, 3, 1),
+    "version": (0, 3, 2),
     "blender": (2, 93, 0),
     "location": "View3D > Tools > CharMorph",
     "description": "Character creation and morphing, cloth fitting and rigging tools",
@@ -37,7 +37,7 @@ bl_info = {
     'tracker_url': 'https://github.com/Upliner/CharMorph/issues',
     "category": "Characters"
 }
-VERSION_ANNEX = "-rc1"
+VERSION_ANNEX = ""
 
 owner = object()
 
